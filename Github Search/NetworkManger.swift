9dc08b3da7b7e064a3f7784de2354cf46c149e6a
khaledmohab1 <kmohab71@yahoo.com>
2021-04-27 16:06:39 +0200

    remove JS implement decoder for the JSON response implement the networking structure

---
+++ Github Search/NetworkManger.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkManger.swift
+//  Github Search
+//
+//  Created by Khaled Mohab on 4/27/21.
+//
+
+import Foundation
+class NetworkManger : ObservableObject{
+    
+    @Published var repos = [RepoData]()
+    
+    func fetechDate( Query:String ){
+        if let url = URL(string: "https://api.github.com/search/repositories?q="+Query) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error == nil{
+                    let decoder = JSONDecoder()
+                    if let safeData = data{
+                        do {
+                            let results = try decoder.decode(Repo.self, from: safeData)
+                            DispatchQueue.main.async {
+                                self.repos = results.items
+                            }
+                            
+                        } catch  {
+                            print(error)
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+}
---
+++ Github Search/Repo.swift
@@ -0,0 +1,22 @@
+//
+//  Repo.swift
+//  Github Search
+//
+//  Created by Khaled Mohab on 4/27/21.
+//
+
+import UIKit
+
+struct Repo: Decodable {
+    let items : [RepoData]
+}
+struct RepoData: Decodable, Identifiable{
+    var id : Int
+    let name: String
+    let description: String
+    let owner: Owner
+}
+struct Owner: Decodable {
+    let login : String
+    let avatar_url : String
+}
